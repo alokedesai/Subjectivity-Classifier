b8f946850762a4d8c7eb447543acb59b0f6ef0a1
Aloke Desai <aloke.desai@gmail.com>
2014-12-05 23:43:44 -0800

    add tf and tfidf to SvmInput; init divide.py

--- working/create_svm_file.py
+++ working/create_svm_file.py
@@ -1,3 +1,4 @@
+import math
 import sys
 
 class SvmInput:
@@ -7,6 +8,21 @@ class SvmInput:
     self.feature_file = open(feature_file, "w")
     self.out_file = open(out_file, "w")
     self.stop = self.read_stoplist(stop_file)
+
+    self.idf = {}
+    for text in self.in_file:
+      line = text.split("\t")[1]
+
+      words_seen = set()
+      for word in line.split(" "):
+
+        word = word.rstrip()
+        if word not in words_seen:
+          self.idf[word] = self.idf.get(word, 0) + 1
+          words_seen.add(word)
+
+    for word in self.idf.keys():
+      self.idf[word] = math.log(len(self.in_file) / self.idf[word])
 
   def read_stoplist(self, stop_file):
     stoplist = open(stop_file, "r").readlines()
@@ -25,35 +41,68 @@ class SvmInput:
   		count[word] = old_count + 1
   	return count
 
+  def get_tf(self, text):
+    count = {}
+    words = text.split(" ")
+    for word in words:
+      word = word.rstrip()
+      old_count = count.get(word, 0)
+      count[word] = old_count + 1
+
+    # normalize by length of text
+    for word in count:
+      count[word] = count[word] / float(len(text))
+    return count
+
+
+  # really inefficient way to implement this, but we'll fix it in the future
+  # when we have some time
+  def get_tfidf(self, text):
+    tf = self.get_tf(text)
+    
+    for word in tf.keys():
+      tf[word] = tf[word] * self.idf[word]
+    return tf
+
+  def create_bow_file(self, feature_type):
+    features = {}
+    feature_count = 1
+
+    for line in self.in_file:
+      line = line.rstrip()
+      label, text = line.split("\t")
+
+      counts = feature_type(text)
+      self.out_file.write(label)
+
+      printme = {}
+      for tok in counts.keys():
+        if (tok not in self.stop):
+          if (tok not in features):
+            features[tok] = feature_count
+            self.feature_file.write(str(feature_count) + "\t" + tok + "\n")
+
+            feature_count+=1
+
+          printme[features[tok]] = counts[tok]
+
+      for fnum in sorted(printme):
+        self.out_file.write(" " + str(fnum) + ":" + str(printme[fnum]))
+      self.out_file.write("\n")
+
+
   def create_word_count_file(self):
-  	features = {}
-  	feature_count = 1
+    self.create_bow_file(self.get_word_counts)
 
-  	for line in self.in_file:
-  		line = line.rstrip()
-  		label, text = line.split("\t")
+  def create_tf_file(self):
+    self.create_bow_file(self.get_tf)
 
-  		counts = self.get_word_counts(text)
-  		self.out_file.write(label)
 
-  		printme = {}
-  		for tok in counts.keys():
-  			if (tok not in self.stop):
-  				if (tok not in features):
-  					features[tok] = feature_count
-  					self.feature_file.write(str(feature_count) + "\t" + tok + "\n")
-
-  					feature_count+=1
-
-  				printme[features[tok]] = counts[tok]
-
-  		for fnum in sorted(printme):
-  			self.out_file.write(" " + str(fnum) + ":" + str(printme[fnum]))
-  		self.out_file.write("\n")
 
 # data_file = sys.argv[1]
 # stoplist = sys.argv[2]
 # out = sys.argv[3]
 
-svm_factory = SvmInput("data/data.txt", "test_feature.txt", "test2.model", "stop.txt")
-svm_factory.create_word_count_file()
+svm_factory = SvmInput("data/data.txt", "test_feature.txt", "model.svm", "stop.txt")
+svm_factory.create_tf_file()
+# svm_factory.create_word_count_file()
